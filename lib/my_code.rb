4f94b6a3a74148adf8e050684b57386d7d2f535a
James Sharpe <jameswrightsharpe@gmail.com>
2019-08-03 23:35:16 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -24,4 +24,28 @@ def map_to_square(array)
   end
 end
 
-def reduce_to_total
+def reduce_to_total(array, index)
+  sum = 0
+  while array[index] do
+    sum += array[index]
+    index++
+  end
+end
+
+def reduce_to_all_true(array)
+  for x in array
+    if(!x)
+      return FALSE
+    end
+  end
+  return TRUE
+end
+
+def reduce_to_any_true(array)
+  for x in array
+    if(x)
+      return TRUE
+    end
+  end
+  return FALSE
+end
